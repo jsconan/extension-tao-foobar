df40dd51706915c327efde98a84e2280115f2458
jsconan <jsconan@gmail.com>
2019-10-01 18:00:38 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return [
     'label' => 'FooBar power extension',
     'description' => 'Dummy test extension',
     'license' => 'GPL-2.0',
-    'version' => '1.2.2',
+    'version' => '1.2.3',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '*'
